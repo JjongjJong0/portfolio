14ce86f490fa98326655cd709855038562f81b42
gksmf7073@outlook.kr <gksmf7073@outlook.kr>
2022-07-12 19:57:02 +0900

    수정중

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(window).ready(function() {
+    //tape
    $(".polaroidBox").hover(function() {
         $(".tape").css("z-index", "-1");
    }, function() {
         $(".tape").css("z-index", "");
    }); 
 
-
-
+    //    
 })
