4d1c903d73b4e1d78e2ea4f57b296512d1e25d8b
gksmf7073@outlook.kr <gksmf7073@outlook.kr>
2022-07-13 13:45:39 +0900

    script입력중

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 $(window).ready(function() {
-    //tape
+     // 모바일(햄버거) 메뉴
+     $(".mobileMenu").click(function() {
+          $(this).toggleClass("open");
+          $(".gnbMobile").toggleClass("hide show");
+     });
+
+    //emoji
    $(".polaroidBox").hover(function() {
         $(".tape").css("z-index", "-1");
+        $(".purpleArrow").css("z-index", "-1");
    }, function() {
         $(".tape").css("z-index", "");
+        $(".purpleArrow").css("z-index", "");
+
    }); 
 
-    //    
+    //modal
+    $(".textBox > strong").click(function() {
+          $("#modal").addClass("on");
+    });
+    $("#modal > .modalUp > .closeBtn").click(function() {
+          $("#modal").removeClass("on");
+     });
 })
